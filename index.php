a9b5c415b0c9300aee6772713b903436940601fe
Riemer Palstra <riemer@palstra.com>
2020-04-06 21:49:35 +0200

    Don't forget to getenv.

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
 
-$dbname = 'OPENSHIFT_MYSQL_DB_NAME';
-$dbuser = 'OPENSHIFT_MYSQL_DB_USERNAME';
-$dbpass = 'OPENSHIFT_MYSQL_DB_PASSWORD';
-$dbhost = 'OPENSHIFT_MYSQL_DB_HOST';
+$dbname = getenv('OPENSHIFT_MYSQL_DB_NAME');
+$dbuser = getenv('OPENSHIFT_MYSQL_DB_USERNAME');
+$dbpass = getenv('OPENSHIFT_MYSQL_DB_PASSWORD');
+$dbhost = getenv('OPENSHIFT_MYSQL_DB_HOST');
 
 $connect = mysqli_connect($dbhost, $dbuser, $dbpass) or die("Unable to connect to the database host!");
 mysqli_select_db($dbname) or die("Could not select the database!");
