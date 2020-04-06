0f66c3d45ed3ef073a0c641fdf30374efebe7f62
Riemer Palstra <riemer@palstra.com>
2020-04-06 22:07:30 +0200

    Add Hello World statement.

--- index.php
+++ index.php
@@ -8,4 +8,6 @@ $dbhost = getenv('OPENSHIFT_MYSQL_DB_HOST');
 $connect = mysqli_connect($dbhost, $dbuser, $dbpass) or die("Unable to connect to the database host!");
 mysqli_select_db($connect, $dbname) or die("Could not select the database!");
 
+echo 'Hello world!';
+
 ?>
